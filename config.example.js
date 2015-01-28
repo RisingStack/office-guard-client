683ad376f0d9605b197bb94ce17c5b83f1a6acf2
Gergely Nemeth <mail@nemethgergely.com>
2015-01-28 17:02:59 +0100

    Adding config.example.js

---
+++ config.example.js
@@ -0,0 +1,41 @@
+var config = {};
+module.exports = config;
+
+config.ports = {
+  ambient: 'B',
+  climate: 'C',
+  accelerometer: 'A'
+};
+
+// possible options for temperature scales: 'c' (Celsius) or 'f' (Fahrenheit)
+config.climate = {
+  scale: 'c'
+};
+
+config.server = {
+  port: 3000
+};
+
+config.alerts = {
+  interval: 15 * 1000, //check them every 15 seconds
+  temperature: {
+    gt: 20,
+    lt: 25
+  },
+  lightLevel: {
+    gt: 0.01,
+    lt: 0.5
+  },
+  soundLevel: {
+    gt: 0.01,
+    lt: 0.1
+  },
+  humidity: {
+    gt: 20,
+    lt: 60
+  }
+};
+
+config.OfficeGuardServer = {
+  url: ''
+};
